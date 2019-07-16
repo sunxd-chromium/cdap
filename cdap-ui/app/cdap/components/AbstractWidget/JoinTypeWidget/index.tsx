52e303db9631ac81d2b4328bb6f20085208e359f
itsanudeep <itsanudeep@google.com>
2019-07-16 15:09:11 -0700

    styled using CSS in JS

--- cdap-ui/app/cdap/components/AbstractWidget/JoinTypeWidget/index.tsx
+++ cdap-ui/app/cdap/components/AbstractWidget/JoinTypeWidget/index.tsx
@@ -14,49 +14,82 @@
  * the License.
  */
 
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
+
 import Select from 'components/AbstractWidget/SelectWidget';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import Paper from '@material-ui/core/Paper';
+
 import ThemeWrapper from 'components/ThemeWrapper';
 import If from 'components/If';
 
-interface IJoinTypeWidgetProps {
+const styles = (theme) => {
+  return {
+    multiCheckboxesContainer: {
+      padding: '20px',
+    },
+    empty: {
+      backgroundColor: 'initial',
+      padding: 'initial',
+      '> h4': {
+        marginBottom: '0px',
+      },
+    },
+    checkboxesGroup: {
+      display: 'flex',
+      'flex-direction': 'column',
+    },
+    subtitle: {
+      color: '#3c4355',
+      fontSize: '13px',
+      'font-weight': '500',
+      marginTop: '15px',
+      marginBottom: '5px',
+    },
+    textWarning: {
+      marginBottom: '5px',
+    },
+    checkbox: {
+      width: '45%',
+      display: 'inline-flex',
+    },
+  };
+};
+
+interface IJoinTypeWidgetProps extends WithStyles<typeof styles> {
   value: string;
-  inputSchema: Array<{name: string}>;
+  inputSchema: Array<{ name: string }>;
   onChange: (arg0: string) => void;
 }
 
-const DROP_DOWN_OPTIONS: Array<string> = ['Inner', 'Outer'];
+const DROP_DOWN_OPTIONS: string[] = ['Inner', 'Outer'];
 
-export default function JoinTypeWidget({
-  value = '',
-  inputSchema,
-  onChange
-}: IJoinTypeWidgetProps) {
+function JoinTypeWidgetView({ value, inputSchema, onChange, classes }) {
   const [joinType, setJoinType] = useState(DROP_DOWN_OPTIONS[1]);
   const [selectedCount, setSelectedCount] = useState(0);
   const [inputs, setInputs] = useState([]);
 
   const formatOutput = () => {
-    const outputArr = inputs.filter(schema => schema.selected).map(schema => schema.name);
+    const outputArr = inputs.filter((schema) => schema.selected).map((schema) => schema.name);
     onChange(outputArr.join(','));
     setSelectedCount(outputArr.length);
   };
 
-  const joinTypeChange = event => {
+  const joinTypeChange = (event) => {
     setJoinType(event.target.value);
     setInputs(
-      inputSchema.map(input => {
-        return {name: input.name, selected: event.target.value === 'Inner'};
+      inputSchema.map((input) => {
+        return { name: input.name, selected: event.target.value === 'Inner' };
       })
     );
   };
 
-  const checkBoxChange = event => {
+  const checkBoxChange = (event) => {
     setInputs(
-      inputs.map(input => {
+      inputs.map((input) => {
         if (input.name === event.target.value) {
           input.selected = !input.selected;
         }
@@ -66,91 +99,101 @@ export default function JoinTypeWidget({
   };
 
   useEffect(() => {
-    const initialModel = value.split(',').map(input => input.trim());
+    const initialModel = value.split(',').map((input) => input.trim());
     if (!value) {
       setInputs(
-        inputSchema.map(input => {
-          return {name: input.name, selected: false};
+        inputSchema.map((input) => {
+          return { name: input.name, selected: false };
         })
       );
     }
     if (initialModel.length === inputSchema.length) {
       setJoinType('Inner');
       setInputs(
-        inputSchema.map(input => {
-          return {name: input.name, selected: true};
+        inputSchema.map((input) => {
+          return { name: input.name, selected: true };
         })
       );
     } else {
       setJoinType('Outer');
       setInputs(
-        inputSchema.map(input => {
+        inputSchema.map((input) => {
           return {
             name: input.name,
-            selected: initialModel.indexOf(input.name) !== -1 ? true : false
+            selected: initialModel.indexOf(input.name) !== -1 ? true : false,
           };
         })
       );
     }
   }, []);
 
-  useEffect(() => {
-    formatOutput();
-  }, [inputs]);
+  useEffect(
+    () => {
+      formatOutput();
+    },
+    [inputs]
+  );
 
   return (
-    <ThemeWrapper>
-      {inputs.length > 0 ? (
-        <div className='multi-checkboxes-container'>
+    <React.Fragment>
+      <If condition={inputs.length > 0}>
+        <Paper className={classes.multiCheckboxesContainer}>
           <Select
             widgetProps={{ values: DROP_DOWN_OPTIONS }}
             value={joinType}
             onChange={joinTypeChange}
           />
           <If condition={joinType === 'Outer'}>
-            <div className='checkboxes-group'>
-              <div className='subtitle'>Required Inputs</div>
+            <div className={classes.checkboxesGroup}>
+              <div className={classes.subtitle}>Required Inputs</div>
               <If condition={selectedCount === inputs.length}>
-                <div className='text-warning'>
+                <div className="text-warning">
                   <span>Setting all stages as required inputs will be treated as Inner Join.</span>
                 </div>
               </If>
-              <div className='row checkboxes'>
+              <div>
                 {inputs &&
-                  inputs.map(input => {
+                  inputs.map((input) => {
                     return (
-                      <div className='col-xs-6 clearfix'>
-                        <FormControlLabel
-                          control={
-                            <Checkbox
-                              checked={input.selected}
-                              value={input.name}
-                              color='primary'
-                              onChange={checkBoxChange}
-                              className='checkbox'
-                            />
-                          }
-                          label={input.name}
-                        />
-                      </div>
+                      <FormControlLabel
+                        className={classes.checkbox}
+                        control={
+                          <Checkbox
+                            checked={input.selected}
+                            value={input.name}
+                            color="primary"
+                            onChange={checkBoxChange}
+                          />
+                        }
+                        label={input.name}
+                      />
                     );
                   })}
               </div>
             </div>
           </If>
-        </div>
-      ) : (
-        <div className='empty'>
+        </Paper>
+      </If>
+      <If condition={inputs.length === 0}>
+        <div className={classes.empty}>
           <h4>No input stages</h4>
         </div>
-      )}
+      </If>
+    </React.Fragment>
+  );
+}
+const StyledJoinTypeWidget = withStyles(styles)(JoinTypeWidgetView);
+
+export default function JoinTypeWidget(props: IJoinTypeWidgetProps) {
+  return (
+    <ThemeWrapper>
+      <StyledJoinTypeWidget {...props} />
     </ThemeWrapper>
   );
-
 }
 
 (JoinTypeWidget as any).propTypes = {
   value: PropTypes.string,
   inputSchema: PropTypes.object,
-  onChange: PropTypes.func
+  onChange: PropTypes.func,
 };
